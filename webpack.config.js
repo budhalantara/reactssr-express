f1c9b95f1ddc63e93b5129cb8cce73ec842ad288
Budha Lantara <budhalantara@gmail.com>
2019-12-13 01:17:42 +0800

    add production

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const glob = require("glob");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
+const development = {
   module: {
     rules: [
       {
@@ -19,5 +22,44 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin()
-  ]
+  ],
+  mode: 'development'
+}
+
+const production = {
+  entry: {
+    "server/index": './server/production.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "[name].js"
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin([{
+      from: `build`, to: 'client'
+    }])
+  ],
+  target: 'node',
+  mode: 'production'
+}
+
+module.exports = (env) => {
+  if(env === 'development') 
+    return development;
+  
+  return production;
 };
